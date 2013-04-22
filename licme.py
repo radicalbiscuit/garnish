73f5a66064398753808dd6fee24d51b2b5c047c8
Jennifer Hamon <jhamon@gmail.com>
2013-04-22 10:21:35 -0700

    Massive changes; minimum working version in place for GPL licenses

--- licme.py
+++ licme.py
@@ -2,10 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import sys
+import datetime
+import os
 
-available_licenses = "artistic agpl3 aparche2 bsd3 gpl2 gpl3 lgpl3 mit mpl2 gpl1 lgpl2 lgpl2.1 mpl1.1 crapl unlicense"
-available_licenses = available_licenses.split(' ')
+CWD = os.getcwd()
 
+LIC_DETAILS = {
+#        'artistic': ['Perl Foudation Artistic License, Version 2.0', 'LICENSE'],
+             'agpl3': ['GNU Affero General Public License, Version 3.0', 'LICENSE'],
+#             'apache2': ['Apache License 2.0', 'LICENSE'],
+#             'bsd3': ['BSD 3-Clause License', 'LICENSE'],
+#             'bsd2': ['BSD 2-Clause License', 'LICENSE'],
+             'gpl2': ['GNU General Public License, Version 2.0', 'LICENSE'],
+             'gpl3': ['GNU General Public License, Version 3.0', 'LICENSE'],
+#             'lgpl3': ['GNU Lesser General Public License, Version 3.0'
+#                 ,'COPYING.LESSER'],
+             'mit': ['MIT License' ,'LICENSE'],
+#             'mpl2': ['Mozilla Public License, Version 2.0', 'LICENSE'],
+             'gpl1': ['GNU General Public License, Version 1.0', 'LICENSE'],
+#             'lgpl2': ['GNU Lesser General Public License, Version 2.0', 'COPYING.LESSER'],
+#             'lgpl2.1': ['GNU Lesser General Public License, Version 2.1', 'COPYING.LESSER'],
+#             'mpl1.1': ['Mozilla Public License, Version 1.1', 'LICENSE'],
+             'crapl': ['Community Research and Academic Programming License', 'LICENSE'],
+#             'unlicense': ['Release code to public domain.  See unlicense.org.', 'UNLICENSE'],
+             'wtfpl': ['Do What the Fuck You Want To Public License, Version 2',
+             'LICENSE']}
 
 parser = argparse.ArgumentParser(prog="LICME",
         description="""
@@ -16,11 +38,6 @@ parser = argparse.ArgumentParser(prog="LICME",
         appended to your existing README file, or one will be created for you
         if it does not already exist.
 
-EXAMPLES
-    licme -l gpl3 -c "Richard Stallman"
-    licme -l crapl -c "Matt Might"
-    licme -l mit -c "Tim the Beaver"
-
         """,
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog=open('licme-list.txt').read()+ """
@@ -30,10 +47,85 @@ CONTRIBUTING
     please submit a pull request at http://www.github.com/jhamon/licme
         """)
 
-parser.add_argument('-l', '--license',
-        help="specify the license you would like to use.", required=True)
-parser.add_argument('-c', '--copyright-holder', type=str, required=True)
-myargs = parser.parse_args()
-print myargs.license
-print myargs.copyright_holder
+parser.add_argument('license',
+    help="specify the license you would like to use.")
+parser.add_argument('copyright_holder',
+    help="specify the author or copyright holder.",
+    type=str)
+parser.add_argument('program_name', help="specify the name of your software")
+parser.add_argument('-y', '--year',
+    help="specify copyright year other than the present year",
+    dest="year",
+    default=str(datetime.date.today().year))
+parser.add_argument('-n', '--no-readme',
+    help="use this option to prevent changes to the README file.",
+    action="store_false",
+    default=True,
+    dest="r")
 
+args = parser.parse_args()
+
+def check_valid_license_choice():
+    if args.license not in LIC_DETAILS.keys():
+        print '''You have entered an invalid license name.  Use -h to see the
+        list of licenses and their abbreviations that licme knows about.'''
+        sys.exit(1)
+
+def already_has_license():
+    """ returns boolean value indicating whether there is
+    already a file in the current working directory that has
+    'license' in the filename
+    """
+    common_license_filenames = ['license',
+                                'copying',
+                                'unlicense',
+                                'copying.lesser',
+                                'license.txt',
+                                'unlicense.txt',
+                                'copying.txt']
+    cwd_files = os.listdir(CWD)
+    possible_licenses = [x for x in cwd_files if x.lower() in
+            common_license_filenames]
+    if len(possible_licenses) != 0:
+        print 'This repository appears to contain license information already.'
+        print 'If you would like to apply a new license using licme, first'
+        print 'remove any license files such as LICENSE, COPYING, or UNLICENSE.'
+        print '\n   Conflicting file: {0}'.format(possible_licenses[0])
+        print '\nNo changes have been made.'
+        sys.exit(1)
+    else:
+        return False
+
+def find_readme():
+    """ Finds the filename of any existing readme (regardless of file extension)
+    and returns the name of the file.  If none is found, 'README' is
+    returned."""
+    readme_file = [x for x in os.listdir(CWD) if x is 'README' or 'readme.' in x.lower()]
+    if len(readme_file) == 0:
+        return 'README'
+    else:
+        return readme_file[0]
+
+def write_readme():
+    with open(find_readme(),'a') as readme:
+        with open(os.path.join('readme-statements',args.license),'r') as ADD_TO_README:
+            text_to_add = ADD_TO_README.read()
+            text_to_add = text_to_add.replace('OWNER_NAME',
+                    args.copyright_holder)
+            text_to_add = text_to_add.replace('COPYRIGHT_YEAR', args.year)
+            text_to_add = text_to_add.replace('PROGRAM_NAME', args.program_name)
+            readme.writelines('\n\n'+ text_to_add)
+
+def install_license():
+    lic_source = os.path.join('licenses',args.license)
+    with open(LIC_DETAILS[args.license][1],'a') as new_license_file:
+        with open(lic_source,'r') as license_source:
+            the_license = license_source.read()
+            new_license_file.write(the_license)
+
+if __name__ == "__main__":
+    check_valid_license_choice()
+    if not already_has_license():
+        install_license()
+        if args.r:
+            write_readme()
