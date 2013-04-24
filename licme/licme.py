d1a25a11ffb3da26cca5df9ed02080ddd9943e52
Jennifer Hamon <jhamon@gmail.com>
2013-04-24 13:20:58 -0700

    Refactor header-related functions.

--- licme/licme.py
+++ licme/licme.py
@@ -186,6 +186,7 @@ def update_readme():
             readme.writelines('\n\n'+ text_to_add)
             print 'Copyright statement added to ' + readme_filename
 
+
 def fill_template(temp):
     """
     Takes a template string (temp) and replaces all template keywords with
@@ -196,6 +197,7 @@ def fill_template(temp):
     temp = temp.replace('PROGRAM_NAME', args.program_name)
     temp = temp.replace('LICENSE_FILENAME', LIC_DETAILS[args.license][1])
     return temp
+
 
 def install_license():
     """
@@ -212,6 +214,7 @@ def install_license():
     if not args.q:
         print LIC_DETAILS[args.license][1] + ' file created.'
 
+
 def install_headers():
     """
     This function handles the installation and removal of in-file licensing
@@ -221,7 +224,7 @@ def install_headers():
     subdirectory, except those files whose "type" cannot be identified from
     filename suffix or files/filetypes that are excluded by the user when
     prompted.  The list of subfiles to be edited is compiled by a different
-    function (list_files_subdirectories).
+    function (list_files_in_subdirs).
 
     If the -r option is used on the command line, install_headers() will remove
     any license notices that have previously been installed by the program.  I
@@ -285,10 +288,10 @@ def install_headers():
         '.ss': ';'
             }
 
-    # Uninstall previously installed header notices
+    # If user has requested with -r option, uninstall previously installed header notices
     if args.remove_headers:
         exclusions = []
-        files_to_fix = list_files_subdirectories(exclusions, comment_chars.keys())
+        files_to_fix = list_files_in_subdirs(exclusions, comment_chars.keys())
         print len(files_to_fix), files_to_fix
         remove_headers_from_files(files_to_fix, comment_chars)
 
@@ -303,85 +306,79 @@ def install_headers():
         else:
             exclusions = []
 
-        files_to_prepend = list_files_subdirectories(exclusions, comment_chars.keys())
-        add_headers_to_files(args, files_to_prepend, comment_chars)
-
-def remove_headers_from_files(files_to_fix, comment_chars):
-    header_messages = {}
-    for file_to_fix in files_to_fix:
-        filetype = re.search('\..+$', file_to_fix).group(0)
-        if filetype in header_messages.keys():
-            remove_header_from_file(file_to_fix, header_messages[filetype])
-        else:
-            header_messages[filetype] = build_header_message(args, filetype, comment_chars)
-            remove_header_from_file(file_to_fix, header_messages[filetype])
-
-def remove_header_from_file(filename, header):
-    with open(filename, 'r') as original_file:
-        original_text = original_file.readlines()
-        beginmsg = ':::::::::::::::: BEGIN LICENSE BLOCK :::::::::::::::'
-        endmsg = '::::::::::::::::  END LICENSE BLOCK  :::::::::::::::'
-
-        start = [k for k,v in enumerate(original_text) if beginmsg in v][0]
-        end = [k for k,v in enumerate(original_text) if endmsg in v][1]
-        if end != []:
-            modified_text = original_text[0:start] + original_text[end+1:]
-            print 'nuhhhhhh'
-        else:
-            modified_text = original_text
-            print 'asdfasdf'
-
-    with open(filename, 'w') as modified_file:
-        modified_file.writelines(modified_text)
+        files_to_prepend = list_files_in_subdirs(exclusions, comment_chars.keys())
+        add_headers_to_files(files_to_prepend, comment_chars)
 
 
-def build_header_message(args, filetype, comment_chars):
-    """
-    Given license information in the args and a file extension string in
-    filetype variable, return a string containing the appropriate in-file
-    licensing header string.
-    """
+def remove_headers_from_files(files_to_fix):
+    for filename in files_to_fix:
+        with open(filename, 'r') as original_file:
+            original_text = original_file.readlines()
+            beginmsg = ':::::::::::::::: BEGIN LICENSE BLOCK :::::::::::::::'
+            endmsg = '::::::::::::::::  END LICENSE BLOCK  :::::::::::::::'
 
-    if filetype in comment_chars.keys():
-        comment_char = comment_chars[filetype]
-    else:
-        print 'Could not add header to {0}. Unknown filetype.'.format(filename)
-        return
+            start = [k for k,v in enumerate(original_text) if beginmsg in v][0]
+            end = [k for k,v in enumerate(original_text) if endmsg in v][0]
+            if end != []:
+                modified_text = original_text[0:start] + original_text[end+1:]
+                print 'nuhhhhhh'
+            else:
+                modified_text = original_text
+                print 'asdfasdf'
 
-    start_msg = comment_char + ' :::::::::::::::: BEGIN LICENSE BLOCK :::::::::::::::'
-    end_msg = comment_char + ' ::::::::::::::::  END LICENSE BLOCK  :::::::::::::::'
-
-    notice = open(os.path.join('header-statements',args.license),'r').readlines()
-    notice = map(fill_template, notice)
-    notice = [comment_char + ' ' + x for x in notice]
-    notice = ''.join(notice)
-
-    complete_header = start_msg + notice + end_msg
-    return complete_header
-
-def add_header_to_file(filename, header_message):
-    # Add a given license header message to the file
-    # with name "filename"
-    with open(filename, 'r') as original:
-        file_contents = original.read()
-    with open(filename, 'w') as modified:
-        modified.write(header_message + file_contents)
+        with open(filename, 'w') as modified_file:
+            modified_file.writelines(modified_text)
 
 
-def add_headers_to_files(args, list_of_files, comment_chars):
+def add_headers_to_files(list_of_files, comment_chars):
     # Take a list of filenames and add an appropriate
     # license header to each one.
+    def add_header_to_file(filename, header_message):
+        """
+        Add a given license header message to the file
+        with name "filename"
+        """
+        with open(filename, 'r') as original:
+            file_contents = original.read()
+        with open(filename, 'w') as modified:
+            modified.write(header_message + file_contents)
+
+    def build_header_message(filetype, comment_chars):
+        """
+        Given license information in the args and a file extension string in
+        filetype variable, return a string containing the appropriate in-file
+        licensing header string.
+        """
+
+        if filetype in comment_chars.keys():
+            comment_char = comment_chars[filetype]
+        else:
+            print 'Could not add header to {0}. Unknown filetype.'.format(filename)
+            return
+
+        start_msg = comment_char + ' :::::::::::::::: BEGIN LICENSE BLOCK :::::::::::::::'
+        end_msg = comment_char + ' ::::::::::::::::  END LICENSE BLOCK  :::::::::::::::'
+
+        notice = open(os.path.join('header-statements',args.license),'r').readlines()
+        notice = map(fill_template, notice)
+        notice = [comment_char + ' ' + x for x in notice]
+        notice = ''.join(notice)
+
+        complete_header = start_msg + notice + end_msg
+        return complete_header
+
+    # Store header messages as they are built using file extension as keys.
     header_messages = {}
     for file_to_check in list_of_files:
         filetype = re.search('\..+$', file_to_check).group(0)
         if filetype in header_messages.keys():
             add_header_to_file(file_to_check, header_messages[filetype])
         else:
-            header_messages[filetype] = build_header_message(args, filetype, comment_chars)
+            header_messages[filetype] = build_header_message(filetype, comment_chars)
             add_header_to_file(file_to_check, header_messages[filetype])
 
 
-def list_files_subdirectories(exclusions, whitelist):
+def list_files_in_subdirs(exclusions, whitelist):
     # Returns a recursive list of all files an all directories,
     # excluding .subdirectories and any explicit exclusions.
     # The exclusions argument should be a list of strings,
