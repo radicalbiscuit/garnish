d5263a1e75af24a6b58ad337424b5272273f456c
Jennifer Hamon <jhamon@gmail.com>
2013-04-30 00:20:22 -0700

    Major changes to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 #!usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+
+from kale.__init__ import __version__ as VERSION
 
 setup(name='kale',
-      version='0.1.0',
-      description='Command-line boilerplate licensing tool',
+      license='MIT License',
+      version=VERSION,
+      description='Command-line tool for adding boilerplate licensing to your open-source projects. GPL, MIT, and BSD licenses supported.',
+      long_description=open('README.md', 'r').read(),
       author='Jennifer Hamon',
       author_email='jhamon@gmail.com',
+      maintainer='Jennifer Hamon',
+      install_requires=['setuptools'],
       url='http://www.github.com/jhamon/kale',
       packages = ['kale'],
-      scripts='kale/kale.py',
       package_data = {'kale' : ['header-statements/*', 'readme-statements/*',
       'licenses/*']},
-      license = 'MIT License'
+      keywords = 'opensource gpl mit bsd licenses licensing',
+      entry_points = {'console_scripts': ['kale=kale.cli:main']}
       )
